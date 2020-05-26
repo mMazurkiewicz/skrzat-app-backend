ee0926ce4aef2d0b766530f0ccafbb4d62b3b34c
Mateusz Mazurkiewicz <mazur@Mazur.local>
2020-05-26 13:37:06 +0200

    init commit, server, fairyTales endpoints and db

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+
+const fairyTalesRoutes = express.Router();
+let FairyTalesModel = require('./fairyTales.model')
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/skrzat-app-db', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+// fairyTales list endpoints
+fairyTalesRoutes.route('/').get(function(req, res) {
+    FairyTalesModel.find(function(err, fairyTales) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(fairyTales);
+        }
+    });
+});
+
+fairyTalesRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    FairyTalesModel.findById(id, function(err, fairyTales) {
+        res.json(fairyTales);
+    });
+});
+
+fairyTalesRoutes.route('/0').post(function(req, res) {
+    let fairyTale = new FairyTalesModel(req.body);
+    fairyTale.save()
+        .then(fairyTale => {
+            res.status(200).json({'fairyTale': 'fairyTale added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new fairyTale failed');
+        });
+});
+
+fairyTalesRoutes.route('/:id').post(function(req, res) {
+    FairyTalesModel.findById(req.params.id, function(err, fairyTale) {
+        if (!fairyTale)
+            res.status(404).send("data is not found");
+        else
+            fairyTale.name = req.body.name;
+            fairyTale.description = req.body.description;
+
+            fairyTale.save().then(fairyTale => {
+                res.json('Fairy Tale updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
+app.use('/fairyTales', fairyTalesRoutes);
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
