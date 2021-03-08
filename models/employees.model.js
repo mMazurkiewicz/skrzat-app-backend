8a3b3ae644c55c2625088130effd0a377820d5e4
Mateusz <mazurkorn@gmail.com>
2021-03-08 17:31:12 +0100

    login route, validation for login&register

--- models/employees.model.js
+++ models/employees.model.js
@@ -2,29 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let EmployeesModel = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    mail: {
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true
-    },
-    // teams: [
-    //   {
-    //     name: {
-    //       type: String,
-    //     },
-    //     _id: {
-    //       type: String,
-    //     }
-    //   }
-    // ],
-    // teams: [{ type: Schema.ObjectId, ref: 'TeamsModel'}]
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  },
+  roles: [{type: String}],
 });
 
 module.exports = mongoose.model('EmployeesModel', EmployeesModel);
