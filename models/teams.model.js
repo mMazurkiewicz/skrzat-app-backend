b337e2fd2b6d890d7cadd4a9f119b6b75a43027a
Mateusz <mazurkorn@gmail.com>
2021-03-22 16:13:17 +0100

    team schema

--- models/teams.model.js
+++ models/teams.model.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-// var MemberSchema = new Schema({ name: String, _id: ObjectId });
-
 let TeamsModel = new Schema({
     name: {
         type: String,
